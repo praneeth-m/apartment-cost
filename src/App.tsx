fe4630d159024f3694e9f8dd58f6287e61786978
Praneeth <praneeth.m@hotmail.com>
2024-09-14 17:06:56 -0500

    Flex price per sft and show parking info

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { TextField, Button } from '@mui/material'
+import { TextField, Button, Select } from '@mui/material'
 
 const baseTheme = createTheme({
   palette: {
@@ -15,20 +15,20 @@ const baseTheme = createTheme({
 })
 
 function App() {
-  const BASE_PRICE = 11200
   const FLOOR_RISE = 20
   const AMENITIES = 400
   const HVAC = 360
-  const PARKING = 1250000
   const DOCS = 10000
   const CORPUS = 100
   const MAINTENANCE = 84
   const GST1 = 5
   const GST2 = 18
 
+  const [priceSqft, setPriceSqft] = useState(11200)
   const [area, setArea] = useState(4365)
   const [floor, setFloor] = useState(20)
   const [cost, setCost] = useState('')
+  const [numParkingSpots, setNumParkingSpots] = useState(3)
 
   const calculateCost = () => {
     if (area < 4365) {
@@ -42,12 +42,23 @@ function App() {
       return
     }
 
+    const calcParkingCost = (area: number) => {
+      if (area <= 4365) {
+        setNumParkingSpots(3)
+        return 125000
+      } else {
+        setNumParkingSpots(4)
+        return 1500000
+      }
+    }
+
     const total1 =
-      area * BASE_PRICE +
+      area * priceSqft +
       (floor - 6) * (FLOOR_RISE * area) +
       area * AMENITIES +
       area * HVAC +
-      PARKING
+      calcParkingCost(area)
+
     const total2 = DOCS + area * CORPUS + area * MAINTENANCE
     const total1GST = (total1 * GST1) / 100
     const total2GST = (total2 * GST2) / 100
@@ -62,6 +73,17 @@ function App() {
       <ThemeProvider theme={baseTheme}>
         <h1 className='m-10'>Grava Cost Calculator</h1>
         <div className='card m-10'>
+          <TextField
+            className='mb-4'
+            type='number'
+            fullWidth
+            id='pricesqft'
+            name='Price per sqft'
+            label='Price/Sqft'
+            variant='outlined'
+            value={priceSqft}
+            onChange={(e) => setPriceSqft(Number(e.target.value))}
+          />
           <TextField
             className='mb-4'
             type='number'
@@ -82,6 +104,7 @@ function App() {
             label='Floor'
             variant='outlined'
             value={floor}
+            InputProps={{ inputProps: { min: 0, max: 10 } }}
             onChange={(e) => setFloor(Number(e.target.value))}
           />
           <Button variant='outlined' type='submit' onClick={calculateCost}>
@@ -95,7 +118,20 @@ function App() {
           >
             Reset
           </Button>
-          {cost && <h1 className='mt-10 text-blue-500'>Rs. {cost}</h1>}
+          {cost && (
+            <div>
+              <h1 className='mt-10 text-blue-500'>Rs. {cost}</h1>
+              <br></br>
+              <h4>
+                Includes{' '}
+                <span style={{ fontSize: '24px', fontWeight: 600 }}>
+                  {numParkingSpots}
+                </span>{' '}
+                parking spots with Option A (2 spots on Basement Level I and{' '}
+                {numParkingSpots - 2} spots on Basement level IV)
+              </h4>
+            </div>
+          )}
         </div>
       </ThemeProvider>
     </>
