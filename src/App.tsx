dd0f3fd9207c5e360f886ac68e32d3a14f77a110
Praneeth <praneeth.m@hotmail.com>
2024-09-15 01:57:33 -0500

    show pricing breakdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { TextField, Button } from '@mui/material'
+import {
+  TextField,
+  Button,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import { styled } from '@mui/material/styles'
 
 const baseTheme = createTheme({
   palette: {
@@ -14,64 +25,99 @@ const baseTheme = createTheme({
   },
 })
 
-function App() {
-  const FLOOR_RISE = 20
-  const AMENITIES = 400
-  const HVAC = 360
-  const DOCS = 10000
-  const CORPUS = 100
-  const MAINTENANCE = 84
-  const GST1 = 5
-  const GST2 = 18
+// consts
+const FLOOR_RISE = 20
+const AMENITIES = 400
+const HVAC = 360
+const DOCS = 10000
+const CORPUS = 100
+const MAINTENANCE = 84
+const GST1 = 5
+const GST2 = 18
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#9a9a9a',
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}))
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}))
+
+function App() {
   const [priceSqft, setPriceSqft] = useState(11200)
   const [area, setArea] = useState(4365)
   const [floor, setFloor] = useState(20)
-  const [cost, setCost] = useState('')
-  const [numParkingSpots, setNumParkingSpots] = useState(3)
+
+  const [finalCost, setFinalCost] = useState(0)
+  const [baseCost, setBaseCost] = useState(0)
+  const [floorRiseCost, setFloorRiseCost] = useState(0)
+  const [parkingCost, setParkingCost] = useState(0)
+  const [amenitiesCost, setAmenitiesCost] = useState(0)
+  const [hvacCost, setHvacCost] = useState(0)
+  const [maintenanceCost, setMaintenanceCost] = useState(0)
+  const [corpusCost, setCorpusCost] = useState(0)
+  const [gst1, setGst1] = useState(0)
+  const [gst2, setGst2] = useState(0)
+  const [numParkingSpots, setNumParkingSpots] = useState(0)
+
+  useEffect(() => {
+    setBaseCost(area * priceSqft)
+    setFloorRiseCost(floor <= 5 ? 0 : (floor - 5) * (FLOOR_RISE * area))
+    setParkingCost(area <= 4365 ? 1250000 : 1500000)
+    setAmenitiesCost(area * AMENITIES)
+    setHvacCost(area * HVAC)
+    setMaintenanceCost(area * MAINTENANCE)
+    setCorpusCost(area * CORPUS)
+    setNumParkingSpots(area <= 4365 ? 3 : 4)
+    setFinalCost(0)
+  }, [area, priceSqft, floor])
 
   const calculateCost = () => {
     if (area < 4365) {
-      setCost('')
       alert('Smallest area is 4365 sqft')
       return
     }
-    if (floor < 1) {
-      setCost('')
+    if (floor < 1 || floor > 50) {
       alert('Invalid floor')
       return
     }
 
-    const calcParkingCost = (area: number) => {
-      if (area <= 4365) {
-        setNumParkingSpots(3)
-        return 125000
-      } else {
-        setNumParkingSpots(4)
-        return 1500000
-      }
-    }
-
     const total1 =
-      area * priceSqft +
-      (floor - 6) * (FLOOR_RISE * area) +
-      area * AMENITIES +
-      area * HVAC +
-      calcParkingCost(area)
+      baseCost + floorRiseCost + amenitiesCost + hvacCost + parkingCost
 
-    const total2 = DOCS + area * CORPUS + area * MAINTENANCE
-    const total1GST = (total1 * GST1) / 100
-    const total2GST = (total2 * GST2) / 100
-    const total = Math.trunc(total1 + total2 + total1GST + total2GST)
-    // setCost(total.toString())
+    const total2 = DOCS + maintenanceCost
+
+    setGst1(Math.trunc((total1 * GST1) / 100))
+    setGst2(Math.trunc((total2 * GST2) / 100))
+
+    const total = Math.trunc(
+      total1 +
+        total2 +
+        (total1 * GST1) / 100 +
+        (total2 * GST2) / 100 +
+        area * CORPUS
+    )
+
     //display price in indian currency format with commas
-    setCost(total.toLocaleString('en-IN')) // 1,23,456
+    setFinalCost(total) // 1,23,456
   }
 
   return (
     <>
       <ThemeProvider theme={baseTheme}>
-        <h1 className='m-10'>Grava Cost Calculator</h1>
+        <h1 className='m-10'>Grava Apartment Pricing</h1>
         <div className='card m-10'>
           <TextField
             className='mb-4'
@@ -114,13 +160,15 @@ function App() {
             className='ml-2'
             variant='outlined'
             type='submit'
-            onClick={() => setCost('')}
+            onClick={() => setFinalCost(0)}
           >
             Reset
           </Button>
-          {cost && (
+          {finalCost > 0 ? (
             <div>
-              <h1 className='mt-10 text-blue-500'>Rs. {cost}</h1>
+              <h1 className='mt-10 text-blue-500'>
+                Rs. {finalCost.toLocaleString('en-IN')}
+              </h1>
               <br></br>
               <h4>
                 Includes{' '}
@@ -131,7 +179,118 @@ function App() {
                 {numParkingSpots - 2} spot{numParkingSpots - 2 > 1 ? 's' : ''}{' '}
                 on Basement level IV)
               </h4>
+              <br></br>
+              <h3>Pricing Breakdown</h3>
+
+              <TableContainer component={Paper}>
+                <Table aria-label='pricing breakdown'>
+                  <TableHead>
+                    <TableRow>
+                      <StyledTableCell>Item</StyledTableCell>
+                      <StyledTableCell>Cost</StyledTableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Base Cost
+                      </TableCell>
+                      <TableCell>
+                        Rs. {baseCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Floor Rise Cost
+                      </TableCell>
+                      <TableCell>
+                        Rs. {floorRiseCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Amenities Cost
+                      </TableCell>
+                      <TableCell>
+                        Rs. {amenitiesCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        HVAC Cost
+                      </TableCell>
+                      <TableCell>
+                        Rs. {hvacCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Parking Cost
+                      </TableCell>
+                      <TableCell>
+                        Rs. {parkingCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Documentation Charges
+                      </TableCell>
+                      <TableCell>Rs. {DOCS.toLocaleString('en-IN')}</TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Corpus Fund
+                      </TableCell>
+                      <TableCell>
+                        Rs. {corpusCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        Maintenance Charges
+                      </TableCell>
+                      <TableCell>
+                        Rs. {maintenanceCost.toLocaleString('en-IN')}
+                      </TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        GST1
+                      </TableCell>
+                      <TableCell>Rs. {gst1.toLocaleString('en-IN')}</TableCell>
+                    </StyledTableRow>
+                    <StyledTableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        GST2
+                      </TableCell>
+                      <TableCell>Rs. {gst2.toLocaleString('en-IN')}</TableCell>
+                    </StyledTableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </div>
+          ) : (
+            ''
           )}
         </div>
       </ThemeProvider>
